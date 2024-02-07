e25be4562907cb40594f6e922ea9e3744be5825f
black-adm <mattheuscontato17@gmail.com>
2024-02-07 15:30:46 -0300

    fix: setup database and created polls route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,24 @@
-import fastify from 'fastify'
+import fastify, { FastifyReply, FastifyRequest } from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
-app.get('/', () => {
-  console.log("Success")
+app.post('/polls', async (
+    request: FastifyRequest, reply: FastifyReply
+  ) => {
+  
+    const createPollBody = z.object({
+      title: z.string()
+    })
+  
+    const { title } = createPollBody.parse(request.body)
+    const poll = await prisma.poll.create({
+      data: { title }
+    })
+
+    return reply.status(201).send({ pollId: poll.id })
 })
 
 app.listen({ port: 3333 }).then(() => {
