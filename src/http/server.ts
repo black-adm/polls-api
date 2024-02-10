e20e284ec38a4ff84d17b8c964e91602a02c4a8d
black-adm <mattheuscontato17@gmail.com>
2024-02-10 12:33:11 -0300

    feat: created web sockets logic from votes

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
 
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 import { voteOnPoll } from './routes/vote-on-poll'
+import { pollResults } from './web-sockets/poll-results'
 
 const app = fastify()
 
@@ -12,9 +14,11 @@ app.register(cookie, {
   hook: "onRequest",
 })
 
+app.register(websocket)
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("⚡ HTTP server is running ...")
